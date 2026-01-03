334e8ed695003db451286023c55a69b667ec94ac
hayem <marc.haye@etu.umontpellier.fr>
2026-01-03 01:33:58 +0100

    generalize setup

--- setup.sh
+++ setup.sh
@@ -1,22 +1,25 @@
 #! /bin/bash
 
-# Update package manager
-sudo dnf update
-
-# Install python
-sudo dnf install python3
-
-# Install python package manager
-sudo dnf install python3-pip
-
-# Install python virtual env
-sudo dnf install python3.13-venv
-
-# Launch virtual env
-python3 -m venv cuda
+# Change it depending on your os
+package_manager="apt"
 
 libs="matplotlib numpy ipykernel jupyter pylzma"
 pytorch_url="https://download.pytorch.org/whl/cu118"
+
+# Update package manager
+sudo $package_manager update
+
+# Install python
+sudo $package_manager install python3
+
+# Install python package manager
+sudo $package_manager install python3-pip
+
+# Install python virtual env
+sudo $package_manager install python3.13-venv
+
+# Launch virtual env
+python3 -m venv cuda
 
 # Install libs in virtual env
 cuda/bin/pip3 install $libs
