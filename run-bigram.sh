5a3acfd85f87dad5d85609ef1246349f252f7dfb
hayem <marc.haye@etu.umontpellier.fr>
2026-03-06 21:20:04 +0100

    Migrate to codeberg

--- run-bigram.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-clear
-# Launch virtual env
-python3 -m venv cuda
-
-# Start bigram model and text generation
-python3 bigram.py
--- run-notebook.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-clear
-# Launch virtual env
-python3 -m venv cuda
-
-# Launch notebook
-jupyter notebook .
